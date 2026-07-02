de64c8663fec801c9a10f0c6a8dee49e1b1a01ba
itzluthfi <luthfishidqi2@gmail.com>
2026-07-02 16:06:00 +0700

    Align Flutter package name to com.danaku.app matching Firebase google-services config

--- DanakuApp/android/app/build.gradle.kts
+++ DanakuApp/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_petama"
+    namespace = "com.danaku.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_petama"
+        applicationId = "com.danaku.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- DanakuApp/android/app/src/main/kotlin/com/danaku/app/MainActivity.kt
+++ DanakuApp/android/app/src/main/kotlin/com/danaku/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app_petama
+package com.danaku.app
 
 import io.flutter.embedding.android.FlutterActivity
 
